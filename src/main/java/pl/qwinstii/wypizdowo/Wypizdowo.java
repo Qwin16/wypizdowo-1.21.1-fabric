cbf93e9ee25371f4d5c246c066a3662dbba9ee77
Qwin16 <bartoszzimny03042008@gmail.com>
2025-06-15 22:34:15 +0200

    Initial commit

---
+++ src/main/java/pl/qwinstii/wypizdowo/Wypizdowo.java
@@ -0,0 +1,16 @@
+package pl.qwinstii.wypizdowo;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Wypizdowo implements ModInitializer {
+	public static final String MOD_ID = "wypizdowo";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/pl/qwinstii/wypizdowo/WypizdowoClient.java
@@ -0,0 +1,10 @@
+package pl.qwinstii.wypizdowo;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class WypizdowoClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
